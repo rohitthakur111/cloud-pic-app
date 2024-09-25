b10fcb1d58252efa8f2bad5a1e61af518f1034bd
Rohit Thakur <159237495+rohitthakur111@users.noreply.github.com>
2024-09-25 17:23:12 +0530

    Update index.js

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios"; 
 
 const Api  = axios.create({
-  baseURL : 'http://127.0.0.1:8080/api/cloud-pic',
+  baseURL : 'https://cloud-pic-app-api.vercel.app',
   headers: {
     ContentType: "application/json",
   }, 
