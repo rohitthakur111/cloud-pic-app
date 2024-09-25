9d9671afaecfb0c94609db963d12ea32d6d6e0f7
rohitthakur111 <rohitthakur.iwdtechnologies@gmail.com>
2024-09-25 17:14:01 +0530

    update vercel.json file

--- server/index.js
+++ server/index.js
@@ -14,8 +14,8 @@ app.use(cors({
 }))
 
 app.use('/api/cloud-pic/images', imageRouter)
-app.use('/', (req,res)=>{
-    res.end("Welcome to the cloud pic!")
+app.get('/', (req,res)=>{
+    res.json("Welcome to the cloud pic!")
 })
 
 module.exports = { app };
