97e62551405de2e88288abb49d96d36dff485f05
Rohit Thakur <159237495+rohitthakur111@users.noreply.github.com>
2024-09-26 14:18:54 +0530

    Update SingleLoading.jsx

--- client/src/components/SingleLoading.jsx
+++ client/src/components/SingleLoading.jsx
@@ -6,15 +6,15 @@ const SingleLoading = () => {
         <div className="md:w-1/2 h-96 md:h-[700px] flex flex-col md:flex-row border shadow rounded-md bg-gray-400 animate-pulse  bg-opacity-50">
         </div>
         <div className="md:w-1/2 md:p-4">
-          <h2 className="w-1/2 flex mb-4 text-xl p-4 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">Image title</h2>
-          <p className="mb-4 p-8 text-gray-50 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">A cow is a domestic animal that is very common, and can be seen often.
+          <h2 className="w-1/2 mb-4 p-4 text-xl text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">Image title</h2>
+          <p className="mb-4 p-8 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">A cow is a domestic animal that is very common, and can be seen often.
 
           </p>
           <div className="flex">
-              <button className="btn  text-white text-uppercase text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50"> Free Download Now
+              <button className="btn text-uppercase text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50"> Free Download Now
               </button>
               <button 
-                  className="btn text-white text-uppercase ms-2 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50"
+                  className="btn text-uppercase ms-2 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50"
                  
               >
                   <span className="text-xl"></span> Delete
