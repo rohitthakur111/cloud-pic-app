5c671e4472e0ef44fb49368f0aea47c98416defc
Rohit Thakur <159237495+rohitthakur111@users.noreply.github.com>
2024-09-26 14:46:07 +0530

    Update multer-upload.js

--- server/middelware/multer-upload.js
+++ server/middelware/multer-upload.js
@@ -1,14 +1,16 @@
 const multer = require('multer')
 
-const storage = multer.diskStorage({
-    destination: (req, file, cb) => {
-      cb(null, './uploads');
-    },
-    filename: (req, file, cb) => {
-      cb(null,  '-' + Date.now() + file.originalname);
-    }
-  });
+// const storage = multer.diskStorage({
+//     destination: (req, file, cb) => {
+//       cb(null, './uploads');
+//     },
+//     filename: (req, file, cb) => {
+//       cb(null,  '-' + Date.now() + file.originalname);
+//     }
+//   });
 
+// const upload = multer({ storage: storage });
+
+const storage = multer.memoryStorage(); // Use memory storage
 const upload = multer({ storage: storage });
-
 module.exports = upload
