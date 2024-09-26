62789c4a7b849b75a9d2f49553aa184d3c2f09b7
Rohit Thakur <159237495+rohitthakur111@users.noreply.github.com>
2024-09-26 16:29:36 +0530

    Update SingleLoading.jsx

--- client/src/components/SingleLoading.jsx
+++ client/src/components/SingleLoading.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 const SingleLoading = () => {
   return (
     <div className='w-full flex flex-col md:flex-row gap-8  my-4 pb-8 border-b items-start'>
-        <div className="md:w-1/2 h-96 md:h-[700px] flex flex-col md:flex-row border shadow rounded-md bg-gray-400 animate-pulse  bg-opacity-50">
+        <div className="w-full md:w-1/2 h-96 md:h-[700px] flex flex-col md:flex-row border shadow rounded-md bg-gray-400 animate-pulse  bg-opacity-50">
         </div>
-        <div className="md:w-1/2 md:p-4">
+        <div className="w-full md:w-1/2 md:p-4">
           <h2 className="w-1/2 mb-4 p-4 text-xl text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">Image title</h2>
           <p className="mb-4 p-8 text-gray-300 bg-gray-400 animate-pulse rounded bg-opacity-50">A cow is a domestic animal that is very common, and can be seen often.
 
