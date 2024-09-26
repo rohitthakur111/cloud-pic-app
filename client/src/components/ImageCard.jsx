954a29ff96201b8d871ec1aa1d2d692ac1fce517
Rohit Thakur <159237495+rohitthakur111@users.noreply.github.com>
2024-09-26 16:18:55 +0530

    Update ImageCard.jsx

--- client/src/components/ImageCard.jsx
+++ client/src/components/ImageCard.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const ImageCard = ({image}) => {
   return (
-    <div className={`card card-compact bg-base-100 w-96 shadow-xl rounded`}>
-      <figure>
-        <img
-          className="h-96 max-h-96 w-full rounded"
-          src={image?.imageUrl} 
-          alt="Shoes" />
-      </figure>
+   <div className="w-96 p-4 md:p-0">
+      <div className={`card card-compact bg-base-100 w-full shadow-xl rounded`}>
+        <figure>
+          <img
+            className="h-96 max-h-96 w-full rounded"
+            src={image?.imageUrl} 
+            alt="Shoes" />
+        </figure>
+      </div>
     </div>
   )
 }
